3c5ce51613f3595d39f47a42186d043899b7d6a0
Grigore Stefan <g_stefan@yahoo.com>
2021-03-15 12:37:42 +0200

    project-structure

--- build/ubuntu.make.sh
+++ build/ubuntu.make.sh
@@ -17,5 +17,5 @@ cmdX(){
 	fi
 }
 
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-shellfind.static.compile
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-shellfind.dynamic.compile
+cmdX xyo-cc --mode=$ACTION @build/source/quantum-script-extension-shellfind.static.compile
+cmdX xyo-cc --mode=$ACTION @build/source/quantum-script-extension-shellfind.dynamic.compile
